c59f494ac82078c9cf6d8d10678d746840a72b59
Hayley Smith <hayley.j.smith@gmail.com>
2024-09-02 14:37:32 -0500

    Center Homepage

--- lib/UI/homepage/home_page.dart
+++ lib/UI/homepage/home_page.dart
@@ -12,14 +12,13 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(),
       drawer: const CustomDrawer(),
       body: const HomepageBody(),
     );
   }
 }
-
 
 class HomepageBody extends StatelessWidget {
   const HomepageBody({
@@ -41,125 +40,130 @@ class HomepageBody extends StatelessWidget {
           tileMode: TileMode.clamp,
         ),
       ),
-      child: ListView(
-        children: [
-          const SizedBox(
-            height: 10,
-          ),
-          const Center(
-            child: Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Image(
-                image: AssetImage(
-                  'assets/ddlogo.png',
+      child: Center(
+        child: Container(
+          width: 600,
+          child: ListView(
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              const Center(
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Image(
+                    image: AssetImage(
+                      'assets/ddlogo.png',
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "Explore a diverse array of debates. From politics to science, ethics to "
-              "entertainment, our platform hosts a rich tapestry"
-              " of intellectual exchange. Stay informed and "
-              "broaden your horizons as you read and absorb "
-              "the diverse viewpoints shared by our "
-              "community.",
-              style: Theme.of(context).textTheme.titleMedium,
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/closed_debates');
-              },
-              child: const Padding(
-                padding: EdgeInsets.all(18.0),
-                child: Text('Debate Collection'),
+              const SizedBox(
+                height: 10,
               ),
-            ),
-          ),
-          const Divider(),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Text(
-                "Challenge ideas, refine arguments, and engage in the art of persuasion by arguing against assertions made by fellow users. Whether you're aiming to test the strength of your own convictions or foster constructive dialogue, Debate Dias provides a platform to articulate your counterarguments. Respectful discourse is the cornerstone of our community, so dive into the discussions and contribute to the intellectual growth of both yourself and others.",
-                style: Theme.of(context).textTheme.titleMedium,
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/open_debates');
-              },
-              child: const Padding(
-                padding: EdgeInsets.all(18.0),
-                child: Text('Join a debate'),
-              ),
-            ),
-          ),
-          const Divider(),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Text(
-                "Have a perspective you're eager to share with the world?"
-                " Craft and present your own assertions. Encourage fellow users to critically analyze your viewpoint, fostering an environment where ideas are explored and refined. Whether your assertion sparks agreement or dissent, your contribution is a vital part of the dynamic discourse taking place on Debate Dias. Join the conversation and let your ideas resonate within our community.",
-                style: Theme.of(context).textTheme.titleMedium,
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/create_debate');
-              },
-              child: const Padding(
-                padding: EdgeInsets.all(18.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
                 child: Text(
-                  "Propose A Debate Topic",
+                  "Explore a diverse array of debates. From politics to science, ethics to "
+                  "entertainment, our platform hosts a rich tapestry"
+                  " of intellectual exchange. Stay informed and "
+                  "broaden your horizons as you read and absorb "
+                  "the diverse viewpoints shared by our "
+                  "community.",
+                  style: Theme.of(context).textTheme.titleMedium,
+                  textAlign: TextAlign.center,
                 ),
               ),
-            ),
-          ),
-          const Divider(),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Text(
-                "Strengthen your argumentative prowess by deepening your understanding of logical fallacies. Our platform offers a dedicated space for you to enhance your critical thinking skills. Familiarize yourself with common pitfalls in reasoning and learn how to identify, avoid, and counteract logical fallacies. Empower yourself to engage in debates with a sharper mind and a more discerning eye. Explore our curated resources and elevate your ability to construct sound, compelling arguments within the realm of rational discourse.",
-                style: Theme.of(context).textTheme.titleMedium,
-                textAlign: TextAlign.center,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/closed_debates');
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.all(18.0),
+                    child: Text('Debate Collection'),
+                  ),
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/logical_fallacies');
-              },
-              child: const Padding(
-                padding: EdgeInsets.all(18.0),
-                child: Text('Learn About Logical Fallacies'),
+              const Divider(),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Text(
+                    "Challenge ideas, refine arguments, and engage in the art of persuasion by arguing against assertions made by fellow users. Whether you're aiming to test the strength of your own convictions or foster constructive dialogue, Debate Dias provides a platform to articulate your counterarguments. Respectful discourse is the cornerstone of our community, so dive into the discussions and contribute to the intellectual growth of both yourself and others.",
+                    style: Theme.of(context).textTheme.titleMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/open_debates');
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.all(18.0),
+                    child: Text('Join a debate'),
+                  ),
+                ),
+              ),
+              const Divider(),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Text(
+                    "Have a perspective you're eager to share with the world?"
+                    " Craft and present your own assertions. Encourage fellow users to critically analyze your viewpoint, fostering an environment where ideas are explored and refined. Whether your assertion sparks agreement or dissent, your contribution is a vital part of the dynamic discourse taking place on Debate Dias. Join the conversation and let your ideas resonate within our community.",
+                    style: Theme.of(context).textTheme.titleMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/create_debate');
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.all(18.0),
+                    child: Text(
+                      "Propose A Debate Topic",
+                    ),
+                  ),
+                ),
+              ),
+              const Divider(),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Text(
+                    "Strengthen your argumentative prowess by deepening your understanding of logical fallacies. Our platform offers a dedicated space for you to enhance your critical thinking skills. Familiarize yourself with common pitfalls in reasoning and learn how to identify, avoid, and counteract logical fallacies. Empower yourself to engage in debates with a sharper mind and a more discerning eye. Explore our curated resources and elevate your ability to construct sound, compelling arguments within the realm of rational discourse.",
+                    style: Theme.of(context).textTheme.titleMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/logical_fallacies');
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.all(18.0),
+                    child: Text('Learn About Logical Fallacies'),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 60,
+              )
+            ],
           ),
-          const SizedBox(
-            height: 60,
-          )
-        ],
+        ),
       ),
     );
   }
